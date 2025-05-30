42720c7e0a174bd175e1407c64e5a5e962ff2b5f
Paulo Henrique PErei <74430831+paulofreyyy@users.noreply.github.com>
2025-05-30 19:27:27 -0300

    feat: criando componente de livro

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header';
 import { Footer } from "./components/footer/footer";
+import { Book } from "./components/book/book";
 
 @Component({
     selector: 'app-root',
-    imports: [HeaderComponent, Footer],
+    imports: [HeaderComponent, Footer, Book],
     templateUrl: './app.html',
     styleUrl: './app.css'
 })
